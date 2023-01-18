9e960277c72f86f3e67c166ee515d24982d2d78c
Dave Geddes <dave.geddes@shopify.com>
2023-01-17 17:06:45 -0700

    latest config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
       files: ["*.mdx"],
       extends: "plugin:mdx/recommended",
       settings: {
-        // "mdx/code-blocks": true,
+        "mdx/code-blocks": true,
         ignoreRemarkConfig: false,
       },
       rules: {
